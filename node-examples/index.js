2701ffb9714726e6619541e3937952a4e0dcc9cf
Tyler Waterman <tylerdwaterman@gmail.com>
2019-09-07 16:09:20 -0400

    Understanding Node Modules

---
+++ node-examples/index.js
@@ -0,0 +1,20 @@
+var rect = {
+	perimeter: (x,y) => (2*(x+y)),
+	area: (x,y) => (x*y)
+};
+
+function solveRectangle(l,b){
+	console.log(`Solving for rectangle with l = ${l} and b = ${b}`)
+
+	if(l<=0 || b<=0){
+		console.log(`Rectangle dimensions should be greater than 0, l = ${l} and b = ${b}`)
+	}else{
+		console.log(`The area of the rectangle is ${rect.area(l,b)}`)
+		console.log(`The perimeter of the rectangle is ${rect.perimeter(l,b)}`)
+	}
+}
+
+solveRectangle(2,4);
+solveRectangle(3,5);
+solveRectangle(0,5);
+solveRectangle(-3, 5);
