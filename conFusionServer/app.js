d4cce225c6da873e6f9b2b17c895ad6a9813157a
Tyler Waterman <tylerdwaterman@gmail.com>
2019-09-16 15:22:22 -0400

    Express Sessions Part 2

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -15,8 +15,6 @@ var leaderRouter = require('./routes/leaderRouter')
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
-const Leaders = require('./models/leaders');
-const Promotions = require('./models/promotions')
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
@@ -44,55 +42,34 @@ app.use(session({
 	store: new FileStore()
 }));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next){
 	console.log(req.session);
 
-	if(!req.session.user){
-		var authHeader = req.headers.authorization;
-
-		if(!authHeader){
-			var err = new Error('You are not authenticated')
-			
-			res.setHeader('WWW-Authenticate', 'Basic');
-			err.status = 401;
-			return next(err);
-		}
-
-		var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-		var username = auth[0];
-		var password = auth[1];
-
-		if(username === "admin" && password === "password"){
-			req.session.user = 'admin';
-			next();
-		}else{
-			var err = new Error('You are not authenticated')
-			
-			res.setHeader('WWW-Authenticate', 'Basic');
-			err.status = 401;
-			return next(err);
-		}
+	if(!req.session.user){		
+		var err = new Error('You are not authenticated')
+		err.status = 403;
+		return next(err);
 	}
 	else{
-		if(req.session.user === 'admin'){
+		if(req.session.user === 'authenticated'){
 			next();
 		}
 		else{
-			var err = new Error('You are not authenticated!');
-
-			err.status = 401;
+			var err = new Error('You are not authenticated')
+			err.status = 403;
 			return next(err);
-		}	
-	}
+		}
+	}	
 }
 
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter)
 app.use('/promotions', promoRouter)
 app.use('/leaders', leaderRouter)
